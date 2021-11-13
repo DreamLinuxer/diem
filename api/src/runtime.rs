929918d4b24f61cf22c0577099cb948393e69153
Xiao Li <ilx@fb.com>
2021-11-12 16:03:56 -0800

    [api] fix default ApiConfig enabled field; add content_length_limit field
    Closes: #9747

--- api/src/runtime.rs
+++ api/src/runtime.rs
@@ -3,7 +3,7 @@
 
 use crate::{context::Context, index};
 
-use anyhow::bail;
+use anyhow::{bail, format_err, Result};
 use diem_config::config::{ApiConfig, JsonRpcConfig, NodeConfig};
 use diem_mempool::MempoolClientSender;
 use diem_types::{chain_id::ChainId, protocol_spec::DpnProto};
@@ -34,10 +34,10 @@ pub fn bootstrap(
     let role = config.base.role;
     let json_rpc_config = config.json_rpc.clone();
     let api_config = config.api.clone();
-    let api = WebServer::from(api_config.clone());
+    let mut api = WebServer::from(api_config.clone());
     let jsonrpc = WebServer::from(json_rpc_config.clone());
-    if api.port() == jsonrpc.port() && api != jsonrpc {
-        bail!("API and JSON-RPC should have same configuration when they are configured to use same port. api: {:?}, jsonrpc: {:?}", api, jsonrpc);
+    if api.port() == jsonrpc.port() {
+        api.merge(&jsonrpc).map_err(|e| format_err!("api and json_rpc are configured using same port with different ip or tls configurations, please configure them same. {}", e))?;
     }
     runtime.spawn(async move {
         let context = Context::new(chain_id, db, mp_sender, role, json_rpc_config, api_config);
@@ -52,7 +52,7 @@ pub fn bootstrap(
 }
 
 #[derive(Clone, Debug, PartialEq)]
-struct WebServer {
+pub(crate) struct WebServer {
     pub address: SocketAddr,
     pub tls_cert_path: Option<String>,
     pub tls_key_path: Option<String>,
@@ -60,27 +60,60 @@ struct WebServer {
 
 impl From<ApiConfig> for WebServer {
     fn from(cfg: ApiConfig) -> Self {
-        Self {
-            address: cfg.address,
-            tls_cert_path: cfg.tls_cert_path,
-            tls_key_path: cfg.tls_key_path,
-        }
+        Self::new(cfg.address, cfg.tls_cert_path, cfg.tls_key_path)
     }
 }
 
 impl From<JsonRpcConfig> for WebServer {
     fn from(cfg: JsonRpcConfig) -> Self {
-        Self {
-            address: cfg.address,
-            tls_cert_path: cfg.tls_cert_path,
-            tls_key_path: cfg.tls_key_path,
-        }
+        Self::new(cfg.address, cfg.tls_cert_path, cfg.tls_key_path)
     }
 }
 
 impl WebServer {
+    pub fn new(
+        address: SocketAddr,
+        tls_cert_path: Option<String>,
+        tls_key_path: Option<String>,
+    ) -> Self {
+        Self {
+            address,
+            tls_cert_path,
+            tls_key_path,
+        }
+    }
+
     pub fn port(&self) -> u16 {
         self.address.port()
+    }
+
+    pub fn ip(&self) -> String {
+        self.address.ip().to_string()
+    }
+
+    pub fn merge(&mut self, other: &WebServer) -> Result<()> {
+        if self.port() != other.port() {
+            bail!("can't merge web server configurations that have different ports");
+        }
+        if self.address.is_ipv6() || other.address.is_ipv6() {
+            bail!("can't merge web server configurations that is IPv6");
+        }
+        if self.ip() != other.ip() {
+            if other.ip() == "0.0.0.0" {
+                self.address.set_ip(other.address.ip())
+            } else if self.ip() != "0.0.0.0" {
+                bail!(
+                    "can't merge web server address IPs: {} {}",
+                    self.ip(),
+                    other.ip()
+                );
+            }
+        }
+        if self.tls_cert_path.is_none() {
+            self.tls_cert_path = other.tls_cert_path.clone();
+            self.tls_key_path = other.tls_key_path.clone();
+        }
+        Ok(())
     }
 
     pub async fn serve<F>(&self, routes: F)
@@ -114,6 +147,8 @@ mod tests {
         runtime::bootstrap,
         tests::{new_test_context, TestContext},
     };
+
+    use super::WebServer;
 
     #[test]
     fn test_bootstrap_failed_if_jsonprc_and_api_configured_same_port_and_different_host() {
@@ -169,6 +204,55 @@ mod tests {
 
         assert_web_server(cfg.api.address.port());
         assert_web_server(cfg.json_rpc.address.port());
+    }
+
+    #[test]
+    fn test_merge_web_server() {
+        let ws1 = WebServer::new("127.0.0.1:8080".parse().unwrap(), None, None);
+        let ws2 = WebServer::new("[::0]:8080".parse().unwrap(), None, None);
+        let ws3 = WebServer::new("[2001:db8::1]:8080".parse().unwrap(), None, None);
+        let ws4 = WebServer::new("0.0.0.0:8080".parse().unwrap(), None, None);
+        let ws5 = WebServer::new("10.10.0.1:8080".parse().unwrap(), None, None);
+        let ws6 = WebServer::new("0.0.0.0:80".parse().unwrap(), None, None);
+        let ws7 = WebServer::new(
+            "0.0.0.0:8080".parse().unwrap(),
+            Some("/cert_path".to_owned()),
+            Some("/key_path".to_owned()),
+        );
+
+        // can't merge ipv6
+        assert!(ws1.clone().merge(&ws2).is_err());
+        assert!(ws1.clone().merge(&ws3).is_err());
+        assert!(ws2.clone().merge(&ws1).is_err());
+        assert!(ws3.clone().merge(&ws4).is_err());
+        assert!(ws4.clone().merge(&ws2).is_err());
+        assert!(ws4.clone().merge(&ws3).is_err());
+
+        // can't merge diff ips
+        assert!(ws1.clone().merge(&ws5).is_err());
+        assert!(ws5.clone().merge(&ws1).is_err());
+
+        // can't merge diff ports
+        assert!(ws4.clone().merge(&ws6).is_err());
+        assert!(ws6.clone().merge(&ws1).is_err());
+        assert!(ws6.clone().merge(&ws7).is_err());
+        assert!(ws1.clone().merge(&ws6).is_err());
+
+        fn assert_merged(mut ws: WebServer, other: &WebServer, address: &str) {
+            ws.merge(other).expect("merged");
+            assert_eq!(ws.address.to_string(), address);
+            assert_eq!(ws.tls_cert_path, other.tls_cert_path);
+            assert_eq!(ws.tls_key_path, other.tls_key_path);
+        }
+        assert_merged(ws1.clone(), &ws1, "127.0.0.1:8080");
+        assert_merged(ws1.clone(), &ws4, "0.0.0.0:8080");
+        assert_merged(ws4.clone(), &ws1, "0.0.0.0:8080");
+        assert_merged(ws4.clone(), &ws5, "0.0.0.0:8080");
+        assert_merged(ws5, &ws4, "0.0.0.0:8080");
+
+        // merge tls cert and key paths
+        assert_merged(ws1, &ws7, "0.0.0.0:8080");
+        assert_merged(ws4, &ws7, "0.0.0.0:8080");
     }
 
     pub fn assert_web_server(port: u16) {
